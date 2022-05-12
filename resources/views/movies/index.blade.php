afc0d026d3aef12f368535d877e43596b1cbb491
patriciabrsk <patriciabresku@gmail.com>
2022-05-12 18:05:26 +0200

    change image placeholder

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Movies')
 
 @section('main-content')
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
             <div class="col-12">
                 <h2 class="text-uppercase">Movies</h2>
             </div>
-            <div class="row row-cols-3 g-4">
+            <div class="row row-cols-4 g-4">
                 @foreach ($movies as $movie)
                 <div class="col">
                     <div class="card">
-                        <img src="http://placehold.jp/100x200.png" class="card-img-top img-fluid" alt="placeholder">
+                        <img src="https://dummyimage.com/300x350/b0b0b0/ffffff&text=Image+Not+Availables" class="card-img-top img-fluid" alt="placeholder">
                         <div class="card-body">
                             <h4 class="card-title">{{ $movie->title }}</h4>
                             <h5 class="card-title">{{ $movie->original_title }}</h5>
@@ -25,6 +25,7 @@
                     </div>
                 </div>
                 @endforeach
+            </div>
         </div>
     </div>
 @endsection
