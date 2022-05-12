3c9d2c8c97736b730e1e2e21bc26f9f33be1864b
patriciabrsk <patriciabresku@gmail.com>
2022-05-12 17:55:21 +0200

    display movie cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -7,10 +7,24 @@
         <div class="row">
             <div class="col-12">
                 <h2 class="text-uppercase">Movies</h2>
-                <p>
-                    @dump($movies)
-                </p>
             </div>
+            <div class="row row-cols-3 g-4">
+                @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card">
+                        <img src="http://placehold.jp/100x200.png" class="card-img-top img-fluid" alt="placeholder">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $movie->title }}</h4>
+                            <h5 class="card-title">{{ $movie->original_title }}</h5>
+                            <p class="card-text">
+                                Nationality: {{ $movie->nationality }}
+                                Release date: {{ $movie->date }}
+                                Rating: {{ $movie->vote }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
         </div>
     </div>
 @endsection
